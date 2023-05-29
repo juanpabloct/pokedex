4ef9313513303850da07021e6d9d1642d40c2507
juanpabloct <juanptorres2003@gmail.com>
2023-05-28 23:46:22 -0500

    modulos de toda la aplicacion y conexion con mongo

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SeedModule } from './seed/seed.module';
+import { CommonModule } from './common/common.module';
+@Module({
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'public'),
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    PokemonModule,
+    SeedModule,
+    CommonModule,
+  ],
+})
+export class AppModule {}
